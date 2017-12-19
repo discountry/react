f91af6480c735e832939bccdf18f1d7eb29275f4
discountry <331027793@qq.com>
2017-12-19 12:32:17 +0800

    rename

--- .scripts/gatsby-deploy.sh
+++ .scripts/gatsby-deploy.sh
@@ -1,11 +1,13 @@
 cd ~/project
 cp -r public/ ../public
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
 git checkout gh-pages
 rm -rf *
 cp -r ../public/* ./
 git add -A
 git commit -m "update docs"
-git push
+git push origin gh-pages
 rm -rf ../public
 
 echo "Finished Deployment!"
